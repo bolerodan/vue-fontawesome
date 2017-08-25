d4e7d92cf179e6945e0b28471ee51059390df169
Dan Wallace <dwallac@mcmaster.ca>
2017-08-25 10:03:13 -0400

    Added in composition support, updated playground

--- src/fa.js
+++ src/fa.js
@@ -1,13 +1,19 @@
 export default {
   props: {
     icon: {
-      required: true
+      required: true,
+      type: String
     },
     prefix: {
       required: false,
+      type: String,
       default: 'fa'
     },
     transform: {
+      required: false,
+      type: String
+    },
+    compose: {
       required: false,
       type: String
     }
@@ -41,23 +47,36 @@ export default {
     getIconDef () {
       const iconDef = this.faAPI.parse.iconFromPack(`${this.prefix} ${this.prefixIconName}`)
       let options = {
-        transform: this.getTransform()
+        transform: this.getTransform(),
+        compose: this.getComposition()
       }
       this.foundIcon = this.faAPI.icon(iconDef, options)
     },
     getTransform () {
       return this.faAPI.parse.transform(this.transform)
+    },
+    getComposition () {
+      if (this.compose) {
+        return this.faAPI.parse.iconFromPack(this.compose)
+      }
+    },
+    getChildren (children, h) {
+      if (!children) return []
+      return children.map((c) => {
+        let key = `${this.prefixIconName}`
+        key = c.attributes ? `${key}-${c.attributes.id}` : key
+        return h(c.tag, {
+          key: key,
+          attrs: c.attributes
+        }, this.getChildren(c.children, h))
+      })
     }
   },
   render (h) {
     if (!this.booted) return // only render when we know FontAwesome is done since it listens for DOMContentLoaded
     const abstract = this.foundIcon.abstract[0]
-    const children = abstract.children.map((c) => {
-      return h(c.tag, {
-        key: this.prefixIconName,
-        attrs: c.attributes
-      })
-    })
+    console.log('ABSTRACT', abstract)
+    const children = this.getChildren(abstract.children, h)
     const svg = h(abstract.tag, {
       key: this.prefixIconName,
       attrs: abstract.attributes,
@@ -66,10 +85,13 @@ export default {
     return svg
   },
   watch: {
-    prefixIconName (val) {
+    prefixIconName () {
       this.getIconDef()
     },
-    transform (val) {
+    transform () {
+      this.getIconDef()
+    },
+    compose () {
       this.getIconDef()
     }
   }
